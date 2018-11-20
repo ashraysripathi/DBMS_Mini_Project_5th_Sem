ddb4b5cd472e778e6fc2c50e7921b811cbdd0a87
ashraysripathi <42443206+ashraysripathi@users.noreply.github.com>
2018-11-20 19:42:01 +0530

    second commit

--- src/Main/LoginPage.java
+++ src/Main/LoginPage.java
@@ -4,8 +4,16 @@
  * and open the template in the editor.
  */
 package Main;
-
+import java.sql.*;
 import java.awt.Color;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,7 +21,7 @@ import java.awt.Color;
  */
 public class LoginPage extends javax.swing.JFrame {
     
-    
+    String login_id;
 
     public LoginPage() {
         initComponents();
@@ -28,13 +36,14 @@ public class LoginPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        title = new javax.swing.JLabel();
         loginid = new javax.swing.JTextField();
         password = new javax.swing.JPasswordField();
-        title = new javax.swing.JLabel();
+        submit = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        focus = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(1920, 1080));
@@ -42,76 +51,159 @@ public class LoginPage extends javax.swing.JFrame {
         setPreferredSize(new java.awt.Dimension(1920, 1080));
         getContentPane().setLayout(null);
 
-        jPanel1.setLayout(null);
+        jPanel2.setLayout(null);
 
-        loginid.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        title.setFont(new java.awt.Font("BlackChancery", 0, 126)); // NOI18N
+        title.setForeground(new java.awt.Color(255, 255, 255));
+        title.setText("L ogin");
+        jPanel2.add(title);
+        title.setBounds(1320, 80, 350, 160);
+
+        loginid.setFont(new java.awt.Font("BlackChancery", 0, 28)); // NOI18N
         loginid.setForeground(new java.awt.Color(204, 204, 204));
-        loginid.setText(" LoginID");
-        loginid.setBorder(javax.swing.BorderFactory.createEmptyBorder(2, 2, 2, 2));
+        loginid.setText("LoginID");
         loginid.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 clear(evt);
             }
         });
-        jPanel1.add(loginid);
-        loginid.setBounds(780, 440, 390, 60);
+        jPanel2.add(loginid);
+        loginid.setBounds(1280, 290, 390, 60);
 
-        password.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        password.setFont(new java.awt.Font("BlackChancery", 0, 28)); // NOI18N
         password.setForeground(new java.awt.Color(204, 204, 204));
         password.setText("Password");
-        password.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         password.setEchoChar((char)0);
         password.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 clear2(evt);
             }
         });
-        jPanel1.add(password);
-        password.setBounds(780, 520, 390, 60);
+        jPanel2.add(password);
+        password.setBounds(1280, 390, 390, 60);
 
-        title.setBackground(new java.awt.Color(255, 255, 255));
-        title.setFont(new java.awt.Font("BlackChancery", 0, 156)); // NOI18N
-        title.setForeground(new java.awt.Color(255, 255, 255));
-        title.setText("L ogin");
-        title.setToolTipText("");
-        jPanel1.add(title);
-        title.setBounds(790, 170, 380, 170);
+        submit.setBackground(new java.awt.Color(0, 0, 0));
+        submit.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        submit.setForeground(new java.awt.Color(255, 255, 255));
+        submit.setText("Submit");
+        submit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitActionPerformed(evt);
+            }
+        });
+        jPanel2.add(submit);
+        submit.setBounds(1300, 500, 350, 60);
 
-        jButton2.setBackground(Color.black);
-        jButton2.setFont(new java.awt.Font("BlackChancery", 0, 48)); // NOI18N
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setFont(new java.awt.Font("BlackChancery", 0, 24)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Submit");
-        jPanel1.add(jButton2);
-        jButton2.setBounds(800, 630, 330, 70);
+        jButton2.setText("New User? Register Here!");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2);
+        jButton2.setBounds(1490, 985, 370, 60);
 
-        jButton3.setBackground(Color.black);
-        jButton3.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("New User? Register Here!");
-        jPanel1.add(jButton3);
-        jButton3.setBounds(1430, 950, 470, 60);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Backgrounds/1013479_christmas-aesthetic-wallpaper.jpg"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(0, 0, 1920, 1080);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Backgrounds/1013479_christmas-aesthetic-wallpaper.jpg"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        jPanel1.add(jLabel1);
-        jLabel1.setBounds(0, 0, 1965, 1080);
+        focus.setText("jTextField1");
+        jPanel2.add(focus);
+        focus.setBounds(490, 250, 69, 22);
 
-        getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 2013, 1080);
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(0, 0, 1920, 1080);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+static final String DB_URL="jdbc:mysql://localhost:3306/resort?autoReconnect=true&useSSL=false";
+static final String USER="root";
+static final String PASS="ashray123";
+Connection con;
+ResultSet rs;
+Statement stmt;
     private void clear(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_clear
-loginid.setText("");  
-loginid.setForeground(Color.black);// TODO add your handling code here:
+ loginid.setText("");
+        loginid.setForeground(Color.black);        // TODO add your handling code here:
     }//GEN-LAST:event_clear
 
     private void clear2(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_clear2
-  password.setText(""); 
-  password.setForeground(Color.black);
-  password.setEchoChar('*');// TODO add your handling code here:
+	
+		 password.setText("");
+        password.setForeground(Color.black);
+        password.setEchoChar('*');        // TODO add your handling code here:
     }//GEN-LAST:event_clear2
+
+    private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
+        login_id=loginid.getText();
+        String pass=password.getText();
+        String usr="",pwd="";
+        
+        if(login_id.substring(0,1)=="ST"){
+            try{
+            Class.forName("java.sql.Driver");
+            con=DriverManager.getConnection(DB_URL,USER,PASS);
+            String staffsql="SELECT STAFFID,PASSWORD FROM CUSTOMER WHERE STAFF='"+login_id+"'AND PASSWORD ='"+pass+"';";
+            }
+            catch(Exception e){
+                
+            }
+        }
+        else{
+        try{
+            Class.forName("java.sql.Driver");
+            con=DriverManager.getConnection(DB_URL,USER,PASS);
+            String sql;
+            sql="SELECT CUSTOMERID,PASSWORD FROM CUSTOMER WHERE CUSTOMERID='"+login_id+"'AND PASSWORD ='"+pass+"';";
+            stmt=con.createStatement();
+            rs=stmt.executeQuery(sql);
+            while(rs.next()){
+                usr=rs.getString("CUSTOMERID");
+                pwd=rs.getString("PASSWORD");
+            }
+            if(login_id.equals(usr)&&pass.equals(pwd)){
+         try{
+              Files.write(Paths.get("C:\\Users\\ashra\\OneDrive\\Documents\\NetBeansProjects\\SylvanGladeResort\\src\\Main\\filename.txt"), login_id.getBytes(), StandardOpenOption.TRUNCATE_EXISTING); 
+            this.setVisible(true);
+            Home h = new Home();
+            h.setVisible(true);
+            this.setVisible(false);
+            }
+            catch(Exception e){
+             JOptionPane.showMessageDialog(this,e.getMessage());
+            }
+            }
+            else{
+                JOptionPane.showMessageDialog(this,"INVALID USER OR PASSWORD");
+                focus.requestFocus();
+            }
+        }
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }  
+        }// TODO add your handling code here:
+    }//GEN-LAST:event_submitActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        this.setVisible(false);
+        Register r = new Register();
+        r.setVisible(true);
+        try{
+               PrintWriter out = new PrintWriter("filename.txt");
+               out.println(login_id);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+     
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
 
     /**
@@ -147,19 +239,23 @@ loginid.setForeground(Color.black);// TODO add your handling code here:
             public void run() {
                 new LoginPage().setVisible(true);
         
-                title.requestFocus();
+                focus.requestFocus();
             }
        
         });
     }
+    public String getCustomer_ID(){
+        return login_id;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private static javax.swing.JTextField focus;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField loginid;
     private javax.swing.JPasswordField password;
+    private javax.swing.JButton submit;
     private static javax.swing.JLabel title;
     // End of variables declaration//GEN-END:variables
 }
--- src/Main/Register.java
+++ src/Main/Register.java
@@ -5,6 +5,9 @@
  */
 package Main;
 
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import java.sql.*;
 /**
  *
  * @author ashra
@@ -27,21 +30,231 @@ public class Register extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        p = new javax.swing.JLabel();
+        cp = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        phone = new javax.swing.JTextField();
+        email = new javax.swing.JTextField();
+        fname = new javax.swing.JTextField();
+        lname = new javax.swing.JTextField();
+        custid = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        address = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        confirmpwd = new javax.swing.JPasswordField();
+        pwd = new javax.swing.JPasswordField();
+        jButton2 = new javax.swing.JButton();
+        bg = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1920, 1080));
+        setMinimumSize(new java.awt.Dimension(1920, 1080));
+        setPreferredSize(new java.awt.Dimension(1920, 1080));
+        getContentPane().setLayout(null);
+
+        jPanel1.setMinimumSize(new java.awt.Dimension(1920, 1080));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1920, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(1974, 0, 1920, 0);
+
+        jLabel2.setFont(new java.awt.Font("BlackChancery", 0, 126)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Register");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(800, 50, 440, 160);
+
+        jLabel3.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("CustomerID");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(80, 240, 220, 70);
+
+        jLabel4.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Address");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(1020, 350, 220, 70);
+
+        p.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        p.setForeground(new java.awt.Color(255, 255, 255));
+        p.setText("Password");
+        p.setToolTipText("");
+        getContentPane().add(p);
+        p.setBounds(80, 340, 220, 70);
+
+        cp.setFont(new java.awt.Font("BlackChancery", 0, 28)); // NOI18N
+        cp.setForeground(new java.awt.Color(255, 255, 255));
+        cp.setText("Confirm Password");
+        getContentPane().add(cp);
+        cp.setBounds(80, 440, 220, 70);
+
+        jLabel7.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("First Name");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(80, 550, 220, 70);
+
+        jLabel8.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("LastName");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(80, 660, 220, 70);
+
+        jLabel9.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("Email");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(80, 770, 220, 70);
+
+        jLabel10.setFont(new java.awt.Font("BlackChancery", 0, 36)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText("Phone");
+        getContentPane().add(jLabel10);
+        jLabel10.setBounds(1030, 240, 220, 70);
+
+        phone.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        getContentPane().add(phone);
+        phone.setBounds(1300, 250, 420, 50);
+
+        email.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        getContentPane().add(email);
+        email.setBounds(310, 780, 420, 50);
+
+        fname.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        fname.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fnameActionPerformed(evt);
+            }
+        });
+        getContentPane().add(fname);
+        fname.setBounds(310, 560, 420, 50);
+
+        lname.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        getContentPane().add(lname);
+        lname.setBounds(310, 670, 420, 50);
+
+        custid.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        getContentPane().add(custid);
+        custid.setBounds(310, 250, 420, 50);
+
+        address.setColumns(20);
+        address.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        address.setRows(5);
+        jScrollPane1.setViewportView(address);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(1300, 360, 420, 250);
+
+        jButton1.setBackground(new java.awt.Color(255, 204, 255));
+        jButton1.setFont(new java.awt.Font("BlackChancery", 0, 48)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(51, 51, 51));
+        jButton1.setText("Register");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(1250, 830, 510, 60);
+
+        confirmpwd.setFont(new java.awt.Font("BlackChancery", 0, 28)); // NOI18N
+        confirmpwd.setEchoChar('*');
+        getContentPane().add(confirmpwd);
+        confirmpwd.setBounds(310, 440, 420, 60);
+
+        pwd.setFont(new java.awt.Font("BlackChancery", 0, 28)); // NOI18N
+        pwd.setEchoChar('*');
+        getContentPane().add(pwd);
+        pwd.setBounds(310, 350, 420, 60);
+
+        jButton2.setFont(new java.awt.Font("BlackChancery", 0, 48)); // NOI18N
+        jButton2.setText("Back");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(1260, 930, 500, 60);
+
+        bg.setFont(new java.awt.Font("BlackChancery", 0, 30)); // NOI18N
+        bg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Backgrounds/registerbg.jpg"))); // NOI18N
+        getContentPane().add(bg);
+        bg.setBounds(0, 0, 1921, 1080);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+static final String DB_URL="jdbc:mysql://localhost/resort";
+static final String USER="root";
+static final String PASS="ashray123";
+Connection con;
+ResultSet rs;
+Statement stmt;
+    private void fnameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fnameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_fnameActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+String cid=custid.getText();
+String password=pwd.getText(); 
+String confpassword=confirmpwd.getText();
+String f_name=fname.getText();
+String l_name=lname.getText();
+String em=email.getText();
+String add=address.getText();
+int ph = Integer.parseInt(phone.getText());
+if(password.equals(confpassword)){
+    try{
+        Class.forName("java.sql.Driver");
+            con=DriverManager.getConnection(DB_URL,USER,PASS);
+            String sql;
+            sql="INSERT INTO CUSTOMER VALUES('"+cid+"','"+password+"','"+f_name+"','"+l_name+"','"+em+"','"+add+"',"+ph+");";
+            stmt=con.createStatement();
+            stmt.executeUpdate(sql);
+            JOptionPane.showMessageDialog(this,"Registration Successful");
+            this.setVisible(false);
+            LoginPage l = new LoginPage();
+            l.setVisible(true);
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+}
+else{
+    JOptionPane.showMessageDialog(this,"Password does not match");
+    cp.setForeground(Color.red);
+    p.setForeground(Color.red);
+}
+
+
+
+
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+LoginPage l =  new LoginPage();
+l.setVisible(true);
+this.dispose();// TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +292,27 @@ public class Register extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea address;
+    private javax.swing.JLabel bg;
+    private javax.swing.JPasswordField confirmpwd;
+    private javax.swing.JLabel cp;
+    private javax.swing.JTextField custid;
+    private javax.swing.JTextField email;
+    private javax.swing.JTextField fname;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField lname;
+    private javax.swing.JLabel p;
+    private javax.swing.JTextField phone;
+    private javax.swing.JPasswordField pwd;
     // End of variables declaration//GEN-END:variables
 }
